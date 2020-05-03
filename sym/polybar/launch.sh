61c65eff6a659daba2a2dfac5f17efca69ce47d6
matterbeam <mb@matterbeam.com>
2020-05-03 16:15:45 +0200

    target update (polybar): pass bars to launch as parameters to the launch.sh script

--- sym/polybar/launch.sh
+++ sym/polybar/launch.sh
@@ -7,14 +7,16 @@ set -eu -o pipefail
 killall -q polybar && while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # launch polybar on all monitors
-for m in $(polybar --list-monitors | cut -d':' -f1); do
-    WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') \
-    MONITOR=$m \
-    SYS_CPU_HWMON_PATH=$(echo /sys/devices/platform/coretemp.0/hwmon/hwmon*/temp1_input) \
-    polybar --reload top &
+for bar in $@; do
+    for m in $(polybar --list-monitors | cut -d':' -f1); do
+        WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') \
+        MONITOR=$m \
+        SYS_CPU_HWMON_PATH=$(echo /sys/devices/platform/coretemp.0/hwmon/hwmon*/temp1_input) \
+        polybar --reload top &
 
-    WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') \
-    MONITOR=$m \
-    SYS_CPU_HWMON_PATH=$(echo /sys/devices/platform/coretemp.0/hwmon/hwmon*/temp1_input) \
-    polybar --reload bottom &
+        WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') \
+        MONITOR=$m \
+        SYS_CPU_HWMON_PATH=$(echo /sys/devices/platform/coretemp.0/hwmon/hwmon*/temp1_input) \
+        polybar --reload bottom &
+    done
 done
